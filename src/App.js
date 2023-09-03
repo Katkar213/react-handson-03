db3022bc3b9104714b7504b3c9d5ce1ce1b0e54e
katkar213 <blackship213@gmail.com>
2023-09-03 15:34:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+import App2 from "./component/App2"
+
+function App() {
+  return (
+    <div className="App">
+      <App2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Data.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react'
+class Data extends Component{
+ 
+    render(){
+        return(
+<>
+<div id="outercontainer">
+{this.props.value.map((item,index)=>{
+    return(
+     
+             <div key={index} id="list">
+            <h3 id="h1">Name: {item.name}||Department: {item.Department}||Rating: {item.Rating}</h3>
+        </div>
+       
+          )
+     })}
+</div>
+<button onClick={this.props.value1}>Go back</button>
+</>
+
+        )
+    }
+}
+export default Data
+
